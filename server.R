d2c6480860e4dae2106aec7a66e427e7b4a6447e
Ron Pat-El <arbanafal@hotmail.com>
2016-04-29 17:28:53 +0200

    stripchart
    In plaats van histogram, een stripchart, zodat een 'histogram' kan worden opgebouwd per steekproef

--- server.R
+++ server.R
@@ -21,8 +21,9 @@ shinyServer(function(input, output) {
     # maak een tabel vol pearson correlaties
     M <- SamplePearson(reps = 1000, N=input$N, rho = input$pears)
     # teken histogram met steekproevenverdeling van rho
-    hist(M, col = 'darkgray', border = 'white', breaks=20, xlim=c(-1,1), prob=T)
-    lines(density(M), col='blue', lwd=2)
+    #hist(M, col = 'darkgray', border = 'white', breaks=20, xlim=c(-1,1), prob=T)
+    stripchart(round(X[1:input$counter], digits=2), method="stack", pch=1, offset=0.8, cex=1, xlim=c(min(X),max(X)), ylim=c(0,10))
+    #lines(density(M), col='blue', lwd=2)
   })
   
   # plot scatterplot
