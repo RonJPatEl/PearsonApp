27bb973c5de69ff5fb71590787b1485fda659546
Ron Pat-El <arbanafal@hotmail.com>
2016-05-03 12:59:47 +0200

    Nieuwe functionaliteit: steekproeven trekken
    Gebruiker kan nu op drie knoppen drukken:
    1) steekproef trekken: trek een extra steekproef
    2) steekproevenverdeling: toont de verdeling van r over 1000 steekproeven
    3) reset: zet steekproeventeller weer op 1
    
    De scatterplot update nu om de meest recente steekproef te tonen. De histogram is vervangen met een stripcahrt waar iedere steekproef met een stip is weergegeven. Dit zou de opbouw vande verdeling dynamischer moeten maken.

--- ui.R
+++ ui.R
@@ -17,8 +17,8 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for correlation 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("pears",
-                   "correlatie:",
+      sliderInput("pears",
+                   "Pearson correlatie in de populatie",
                    min = -.99,
                    max = .99,
                    value = .60),
@@ -26,13 +26,23 @@ shinyUI(fluidPage(
                   "Steekproefgrootte:",
                   min = 10,
                   max = 1000,
-                  value = 500)
-    ),
-    
+                  value = 500),
+      # Knop om steeds een extra steekproef mee te trekken
+      actionButton("counter", label = "Trek een Steekproef"),
+      
+      # Resetknop om counter mee op nul te zetten
+      actionButton("reset", label = "Reset"),
+      
+      # Knop om de volledige steekproevenverdeling te tonen
+      actionButton("sampling", label= "Trek steekproevenverdeling")
+      
+       ),
+   
     # Show a plot of the generated distribution
     mainPanel(
        plotOutput("distPlot"),
-       plotOutput("Scatter")
+       plotOutput("Scatter"),
+       uiOutput("count") #tijdelijk: counterteller van steekproefknop
     )
   )
 ))
