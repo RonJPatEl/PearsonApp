8ebb95823669ae0dcde501b0823034a3aeae86fb
dr. Ron J PatEl <arbanafal@hotmail.com>
2016-05-19 18:01:22 +0200

    Revert "ggplot, grafische aanpassing"
    This reverts commit 691ea6ad9c1bd7747b392cf4f4c271cfbcdd791d.

--- ui.R
+++ ui.R
@@ -15,43 +15,34 @@ shinyUI(fluidPage(
   titlePanel("Steekproevenverdeling van de pearson correlatie"),
   
   # Sidebar with a slider input for correlation 
-  fluidRow(
-    column(4,
-    wellPanel(
-      helpText("Deze slider bepaalt welke pearson correlatie er tussen twee variabelen is in de gehele populatie"),
+  sidebarLayout(
+    sidebarPanel(
       sliderInput("pears",
                    "Pearson correlatie in de populatie",
                    min = -.99,
                    max = .99,
                    value = .60),
-      
-      helpText("Met deze slider wordt de steekproefgrootte van iedere steekproef aangepast"),
       sliderInput("N",
                   "Steekproefgrootte:",
                   min = 10,
-                  max = 500,
-                  value = 100),
-      
+                  max = 1000,
+                  value = 500),
       # Knop om steeds een extra steekproef mee te trekken
-      actionButton("counter", label = "Trek een Steekproef", icon=icon("fa fa-step-forward")),
-      
-            # Knop om de volledige steekproevenverdeling te tonen
-      actionButton("sampling", label= "Trek steekproevenverdeling", icon=icon("fa fa-fast-forward")),
+      actionButton("counter", label = "Trek een Steekproef"),
       
       # Resetknop om counter mee op nul te zetten
-      actionButton("reset", label = "Reset", icon=icon("fa fa-refresh")),
+      actionButton("reset", label = "Reset"),
       
-      helpText("U kunt een maximum van 1000 steekproeven trekken. Als u de knop [Trek steekproevenverdeling]
-               heeft ingedrukt zult u eerst moeten resetten voordat u weer individuele steekproeven kunt trekken ")
+      # Knop om de volledige steekproevenverdeling te tonen
+      actionButton("sampling", label= "Trek steekproevenverdeling")
       
-       )),
+       ),
    
     # Show a plot of the generated distribution
-    column(4,
-       plotOutput("distPlot", height="800px")),
-    column(4,
-       plotOutput("Scatter", height="800px")
-       #uiOutput("count") #tijdelijk: counterteller van steekproefknop
+    mainPanel(
+       plotOutput("distPlot"),
+       plotOutput("Scatter"),
+       uiOutput("count") #tijdelijk: counterteller van steekproefknop
     )
   )
 ))
